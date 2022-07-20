ae85eed81a7a9c9a68bcd5e9018e04dd9e055405
Tobias Hienzsch <tobias@modern-circuits.com>
2022-07-20 22:15:15 +0200

    More markdown to html tests

--- markdown_test.go
+++ markdown_test.go
@@ -22,3 +22,18 @@ func TestMarkdownToHTML(t *testing.T) {
 	assert.NotEmpty(t, h2)
 	assert.Contains(t, string(h2), "<h2>Foo</h2>")
 }
+
+func TestMarkdownFileToHTML(t *testing.T) {
+	noexist := paket.MarkdownFileToHTML("/noexist/noexist.md")
+	assert.Empty(t, noexist)
+
+	file := paket.MarkdownFileToHTML("testdata/txt/simple_markdown.md")
+	assert.NotEmpty(t, file)
+	assert.Contains(t, string(file), "ASIC Filter")
+	assert.Contains(t, string(file), "Foo Bar Baz")
+
+	formatted := paket.FormatHTML(file)
+	assert.NotEmpty(t, formatted)
+	assert.Contains(t, string(formatted), "ASIC Filter")
+	assert.Contains(t, string(formatted), "Foo Bar Baz")
+}
