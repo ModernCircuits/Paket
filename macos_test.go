9a5b6d75ff84f7e85422c95ef86d0e0fd6abf0eb
Tobias Hienzsch <tobias@modern-circuits.com>
2022-07-20 16:41:48 +0200

    Add macOS tests

---
+++ macos_test.go
@@ -0,0 +1,25 @@
+package paket
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreateMacInstaller(t *testing.T) {
+
+	path := "testdata/minimal.hcl"
+
+	project, err := NewProject(path)
+	assert.NoError(t, err)
+
+	script, err := createMacInstaller(*project)
+	assert.NoError(t, err)
+	assert.NotNil(t, script)
+	assert.Equal(t, "Plugin Template", script.Title)
+	assert.Empty(t, script.License)
+	assert.Empty(t, script.Welcome)
+	assert.Empty(t, script.Conclusion)
+	assert.Len(t, script.Choices, 1)
+
+}
